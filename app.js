6798a90c1785321adf024cb70951a33ae6b6736a
Paul Milbank <paulmilbank@gmail.com>
2018-01-01 13:05:27 +1300

    simplify app.js and fix helmet usage

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express')
-const path = require('path')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const helmet = helmet = require('helmet')
+const helmet = require('helmet')
 
 const index = require('./routes/index')
 
 const app = express()
 
-app.use(helmet)
+app.use(helmet())
 app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', index)
@@ -32,7 +28,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.json({error: res.locals.message})
 })
 
 module.exports = app
