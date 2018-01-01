ca9b5a6c514ee603668c79bdc77925854cda943c
Paul Milbank <paulmilbank@gmail.com>
2018-01-01 13:07:08 +1300

    mucking around with query parameters and returns.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.json({'response': 'Yes'}).status(200)
+const urlEncodedParser = bodyParser.urlencoded({ extended: true })
+
+router.get('/', urlEncodedParser, (req, res, next) => {
+  if (!req.body) return res.status(400)
+
+  const userName = req.query.username
+  console.dir(userName)
+
+  res.json({welcome: userName}).status(200)
 })
 
 module.exports = router
