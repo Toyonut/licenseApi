bb368a64ab708c71af75ddbfe6f5d4b99f0a5a23
Paul Milbank <paulmilbank@gmail.com>
2018-02-22 20:37:41 +1300

    repeat license name back and fail if no name

--- routes/license.js
+++ routes/license.js
@@ -6,13 +6,14 @@ const router = express.Router()
 /* GET home page. */
 
 router.get('/:licenseName/', (req, res, next) => {
-  if (!req.body) return res.status(400)
-
   const licenseName = req.params.licenseName
-
-  res.status(200).json({
-    licenseName: licenseName
-  })
+  if (licenseName) {
+    res.status(200).json({
+      licenseName: licenseName
+    })
+  } else {
+    res.status(400)
+  }
 })
 
 module.exports = router
