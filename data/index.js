690720a652bd7f4d271f86ebbff7dbda9e4fd592
Paul Milbank <paulmilbank@gmail.com>
2018-02-26 19:32:34 +1300

    instantiate db class away from definition add getLicenseAsync method

--- data/index.js
+++ data/index.js
@@ -1,6 +1,17 @@
 'use strict'
 const LicenseMethods = require('./dataAccess')
+const pgp = require('pg-promise')()
 
-const Licenses = new LicenseMethods()
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'licensedata',
+  user: 'nodeuser',
+  password: 'password'
+}
+
+const db = pgp(config)
+
+const Licenses = new LicenseMethods(db)
 
 module.exports = Licenses
