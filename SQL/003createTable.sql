4e6281826d5bc97ba343b99ce98c45a53aa6a033
Paul Milbank <paulmilbank@gmail.com>
2018-05-06 19:05:16 +1200

    Uppercasing and make license_name longer.

--- SQL/003createTable.sql
+++ SQL/003createTable.sql
@@ -1,11 +1,11 @@
-drop table if exists license_info;
+DROP TABLE IF EXISTS license_info;
 
-CREATE TABLE If NOT EXISTS license_info (
-  id serial PRIMARY KEY,
-  license_name varchar(45) not null,
+CREATE TABLE IF NOT EXISTS license_info (
+  id SERIAL PRIMARY KEY,
+  license_name VARCHAR(100) not null,
   license_text TEXT not null,
-  license_url varchar(200) not null,
-  license_short_name varchar(40)
+  license_url VARCHAR(200) not null,
+  license_short_name VARCHAR(40)
 );
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO nodeuser;
