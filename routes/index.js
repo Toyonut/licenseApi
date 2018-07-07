7650c700902f0bc71328c2c086869fc269d615f5
Paul Milbank <paulm@outlook.co.nz>
2018-07-07 23:31:06 +1200

    just ship html instead of ejs template

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@
 
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({'response': 'Hello World'})
+  res.sendFile(path.join(__dirname, '..', '/public/html/index.html'))
 })
 
 module.exports = router
